b0a2a690efb9afbd315d590c0293601b6c1a4ffd
Bret Mckee <bret.mckee@gmail.com>
2025-11-14 23:21:15 -0800

    After task 1.4

---
+++ shared/cache/ttl.py
@@ -0,0 +1,30 @@
+# Copyright 2025 Bret McKee (bret.mckee@gmail.com)
+#
+# This file is part of Game_Scheduler. (https://github.com/game-scheduler)
+#
+# Game_Scheduler is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Game_Scheduler is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with Game_Scheduler If not, see <https://www.gnu.org/licenses/>.
+
+
+"""Cache TTL configuration constants."""
+
+
+class CacheTTL:
+    """Time-to-live (TTL) constants for cache entries in seconds."""
+
+    DISPLAY_NAME = 300  # 5 minutes
+    USER_ROLES = 300  # 5 minutes
+    SESSION = 86400  # 24 hours
+    GUILD_CONFIG = 600  # 10 minutes
+    CHANNEL_CONFIG = 600  # 10 minutes
+    GAME_DETAILS = 60  # 1 minute
---
+++ tests/shared/cache/test_keys.py
@@ -0,0 +1,60 @@
+# Copyright 2025 Bret McKee (bret.mckee@gmail.com)
+#
+# This file is part of Game_Scheduler. (https://github.com/game-scheduler)
+#
+# Game_Scheduler is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Game_Scheduler is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with Game_Scheduler If not, see <https://www.gnu.org/licenses/>.
+
+
+"""Unit tests for cache key patterns."""
+
+from shared.cache.keys import CacheKeys
+
+
+class TestCacheKeys:
+    """Test suite for CacheKeys pattern generation."""
+
+    def test_display_name_key(self):
+        """Test display name cache key generation."""
+        key = CacheKeys.display_name("guild123", "user456")
+        assert key == "display:guild123:user456"
+
+    def test_user_roles_key(self):
+        """Test user roles cache key generation."""
+        key = CacheKeys.user_roles("user456", "guild123")
+        assert key == "user_roles:user456:guild123"
+
+    def test_session_key(self):
+        """Test session cache key generation."""
+        key = CacheKeys.session("session_abc123")
+        assert key == "session:session_abc123"
+
+    def test_guild_config_key(self):
+        """Test guild config cache key generation."""
+        key = CacheKeys.guild_config("guild123")
+        assert key == "guild_config:guild123"
+
+    def test_channel_config_key(self):
+        """Test channel config cache key generation."""
+        key = CacheKeys.channel_config("channel789")
+        assert key == "channel_config:channel789"
+
+    def test_game_details_key(self):
+        """Test game details cache key generation."""
+        key = CacheKeys.game_details("game_uuid_123")
+        assert key == "game:game_uuid_123"
+
+    def test_oauth_state_key(self):
+        """Test OAuth state cache key generation."""
+        key = CacheKeys.oauth_state("state_random123")
+        assert key == "oauth_state:state_random123"
